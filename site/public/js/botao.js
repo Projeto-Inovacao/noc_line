3b3e2999b8cbea885f82dc4b963511f7244e487d
Yasmya <yasmin.kakoi@sptech.school>
2023-08-15 19:18:12 -0300

    Parte de baixo do site

---
+++ site/public/js/botao.js
@@ -0,0 +1,12 @@
+// Função pra redirecionar pras paginas..
+function home() {
+  window.location = 'index.html'
+}
+
+function login() {
+  window.location = 'login.html'
+}
+
+function cadastro() {
+  window.location = 'cadastro.html'
+}
