7ec58023d22859662f8fb7e13be897131dd409af
Yasmya <yasmin.kakoi@sptech.school>
2023-09-08 20:28:24 -0300

    melhoria cadastro + alerta

--- site/public/js/botao.js
+++ site/public/js/botao.js
@@ -4,9 +4,25 @@ function home() {
 }
 
 function login() {
-  window.location = 'login.html'
+  var token = input_token.value
+  var senha = input_senha.value
+
+  if (token != '' && senha != '') {
+    Swal.fire({ icon: 'success', title: 'Login realizado com sucesso!' })
+  } else {
+    Swal.fire({ icon: 'error', title: 'Login inválido!' })
+  }
 }
 
 function cadastro() {
-  window.location = 'cadastro.html'
+  var razao_social = input_razao_social.value
+  var cnpj = input_cnpj.value
+  var email = input_email.value
+  var senha = input_senha.value
+
+  if (razao_social != '' && cnpj != '' && email != '' && senha != '') {
+    Swal.fire({ icon: 'success', title: 'Cadastro realizado com sucesso!' })
+  } else {
+    Swal.fire({ icon: 'error', title: 'Cadastro inválido!' })
+  }
 }
