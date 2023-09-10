152d92735aa80dd2c1fbe9a0339f882c38081fd6
iHx7z <anthony.ferreira@sptech.school>
2023-09-10 12:48:53 -0300

    kotlin da galera

---
+++ kotlin/sprintTony/src/main/kotlin/CadastroEmpresa.kt
@@ -0,0 +1,9 @@
+import java.lang.invoke.StringConcatFactory
+
+class CadastroEmpresa () {
+    var razaoSocial:String = ""
+    var CNPJ:String = ""
+    var email:String = ""
+    var senha:String = ""
+    var confirmarSenha:String = ""
+}
---
+++ kotlin/sprintTony/src/main/kotlin/CadastroFuncionario.kt
@@ -0,0 +1,7 @@
+class CadastroFuncionario () {
+    var nome:String = ""
+    var CPF:String = ""
+    var tipoAcesso:String = ""
+    var senha:String = ""
+    var confirmarSenha:String = ""
+}
